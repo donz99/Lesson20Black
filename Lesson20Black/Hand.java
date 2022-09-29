176e4c2db8ed9306301476c0dfe5256473f816cf
DenisK <35003466+donz99@users.noreply.github.com>
2022-09-29 21:28:17 +0300

    Add files via upload

---
+++ Lesson20Black/Hand.java
@@ -0,0 +1,31 @@
+package Lesson20Black;
+
+import java.util.ArrayList;
+
+public abstract class Hand {
+    protected ArrayList<Card> hand;
+    Hand(){
+        hand = new ArrayList<>();
+    }
+    public void takeCard(Deck deck){
+        hand.add(deck.sendCard());
+    }
+
+    public String toString(){
+        String res = "";
+        for (int i = 0; i < hand.size(); i++) {
+            res += hand.get(i).toString();
+        }
+        return res;
+    }
+    public int calcScore(){
+        //TODO сделать что бы туз давал 1 или 11 очков
+        int score = 0;
+        for (Card card:
+             hand) {
+            score += card.getRank();
+        }
+        return score;
+    }
+    public abstract void play();
+}
---
+++ Lesson20Black/Main.java
@@ -0,0 +1,9 @@
+package Lesson20Black;
+
+public class Main {
+    public static void main(String[] args) {
+        //Карта, Колода, игрок, дилер, гейм
+        Deck deck = new Deck();
+        System.out.println(deck);     //отладочный код
+    }
+}
